06629796700234486e137e9f40e995998520cf44
William Rodriguez <wisrovi.rodriguez@gmail.com>
2025-03-27 08:24:25 +0100

    Update install_worker.sh

--- production/install/worker/install_worker.sh
+++ production/install/worker/install_worker.sh
@@ -1,2 +1,2 @@
-curl -o Makefile https://raw.githubusercontent.com/wisrovi/wyoloservice/refs/heads/main/production/2%20train_Server/Makefile
+curl -o Makefile "https://raw.githubusercontent.com/wisrovi/wyoloservice/refs/heads/main/production/2%20train_Server/Makefile"
 make start_worker
